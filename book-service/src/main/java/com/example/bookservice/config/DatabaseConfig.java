f851313971d13ed7b4b978378adde5d4badf9ee2
evlazutin <ev.lazutin@gmail.com>
2023-12-20 18:45:00 +0500

    fix

---
+++ book-service/src/main/java/com/example/bookservice/config/DatabaseConfig.java
@@ -0,0 +1,32 @@
+package com.example.bookservice.config;
+
+import com.example.bookservice.model.Book;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@AllArgsConstructor
+public class DatabaseConfig {
+
+    private final MongoTemplate mongoTemplate;
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            // Создаем список книг для сохранения в базу данных
+            List<Book> books = Arrays.asList(
+                    Book.builder().title("Book 1").description("Description 1").imageLink("Link 1").build(),
+                    Book.builder().title("Book 2").description("Description 2").imageLink("Link 2").build(),
+                    Book.builder().title("Book 3").description("Description 3").imageLink("Link 3").build()
+            );
+            // Сохраняем книги в базу данных
+            mongoTemplate.insertAll(books);
+        };
+    }
+}
