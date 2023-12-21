eb6a88105297ec871a9242090d954d2189e943ae
evlazutin <ev.lazutin@gmail.com>
2023-12-21 15:49:34 +0500

    LB + CircuitBreaker

--- book-service/src/main/java/com/example/bookservice/controller/BookRestController.java
+++ book-service/src/main/java/com/example/bookservice/controller/BookRestController.java
@@ -3,6 +3,7 @@ package com.example.bookservice.controller;
 import com.example.bookservice.model.Book;
 import com.example.bookservice.service.BookService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,26 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
-@AllArgsConstructor
+//@AllArgsConstructor
 public class BookRestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String lb;
+
+    public BookRestController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     private final BookService bookService;
 
     @GetMapping
     public List<Book> getAllBooks(){
         return bookService.getAllBooks();
+    }
+
+    @GetMapping("/lb")
+    public String getLb(){
+        return lb;
     }
 
 }
--- user-service/src/main/java/com/example/userservice/connector/BookServiceConnector.java
+++ user-service/src/main/java/com/example/userservice/connector/BookServiceConnector.java
@@ -1,15 +1,26 @@
 package com.example.userservice.connector;
 
 import com.example.userservice.model.Book;
+import feign.Feign;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collections;
 import java.util.List;
 
 @FeignClient(name = "book-service")
+@CircuitBreaker(name = "book-service", fallbackMethod = "fallback")
 public interface BookServiceConnector {
 
     @GetMapping("/api/books")
     List<Book> getAllBooks();
 
+    default List<Book> fallback(Throwable ex) {
+        final Logger log = LogManager.getLogger(Feign.class);
+        log.info(ex.getMessage());
+        return Collections.emptyList();
+    }
 }
